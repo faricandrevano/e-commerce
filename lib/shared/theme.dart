8fe436d298a0bed0956e186630f34980ea955d0c
faricandrevano <faric26andrevano@gmail.com>
2024-07-05 21:11:28 +0700

    first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color lightBackgroundGreen = const Color(0xffE4F3EA);
+Color lightBackgroundYellow = const Color(0xffFFF6E4);
+Color lightBackgroundOrange = const Color(0xffFFECE8);
+Color lightBackgroundPurple = const Color(0xffF1EDFC);
+
+Color blueColor = const Color(0xff3669C9);
+Color yellowColor = const Color(0xffFFC120);
+Color greenColor = const Color(0xff3A9B7A);
+Color navyColor = const Color(0xff13181F);
+Color whiteColor = const Color(0xffFFFFFF);
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: const Color(0xffFFFFFF),
+);
+
+TextStyle blackColorStyle = GoogleFonts.dmSans(
+  color: const Color(0xff000000),
+);
+TextStyle greyColorStyle = GoogleFonts.dmSans(
+  color: const Color(0xff838589),
+);
+TextStyle blueColorStyle = GoogleFonts.dmSans(
+  color: const Color(0xff3669C9),
+);
+TextStyle redColorStyle = GoogleFonts.dmSans(
+  color: const Color(0xffFE3A30),
+);
+TextStyle greenColorStyle = GoogleFonts.dmSans(
+  color: const Color(0xff3A9B7A),
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
